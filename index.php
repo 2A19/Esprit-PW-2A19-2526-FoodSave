c8ae28867dd1588eb6540aba0e6872eb16708cfb
Faten-karouii <faten.karoui@esprit.tn>
2026-04-13 22:55:39 +0100

    Delete index.php

--- index.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Redirection vers le FrontOffice de l'application.
-// Ouvrir toujours via un serveur HTTP (localhost), pas en double-cliquant sur le fichier.
-header('Location: app/views/front/accueil.php');
-exit;
